9650e1d82cfb8846ed1e8fa61fc087693a7c9ba4
Your GitHub Username <davilama@unlv.nevada.edu>
2024-08-11 09:43:29 -0700

    finished HW

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,60 @@
 
 // Your Code Here
+
+// used this area to write down the code for part 2
+
+// let response = await fetch ('http://localhost:3001/updateBook', {
+//     method: "PATCH",
+//     headers: {
+//         'Content-Type': 'application/json'
+//     },
+//     body:JSON.stringify({
+//         "id": 3,
+//         "title": "Legends of Arathrae",
+//     }),
+// });
+// let updateBook = await response.json();
+// console.log(updateBook)
+
+// end of part 2
+
+/* start of part 3 */
+console.log("hi"); /* testing log */
+
+async function main () {
+    let response = await fetch('http://localhost:3001/listBooks');
+    let books = await response.json();
+    books.forEach(renderBook);
+}
+
+function renderBook(book) {
+    let root = document.querySelector('#root');
+
+    let li = document.createElement('li');
+    li.textContent = book.title;
+
+    let quantityInput = document.createElement('input');
+    quantityInput.value = book.quantity;
+
+    let saveButton = document.createElement('button');
+    saveButton.textContent = 'save';
+
+    saveButton.addEventListener('click', () =>{
+        fetch('http://localhost:3001/updateBook', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify({
+                id: book.id,
+                quantity: quantityInput.value
+            })
+        })
+    })
+
+    li.append(quantityInput, saveButton);
+
+    root.append(li);
+}
+
+main();
